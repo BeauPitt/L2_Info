20fa1ef476e0fbc208edce62c80160f7f9dea58e
BeauPitt <loriscrc@gmail.com>
2016-03-21 15:29:44 +0100

    ptitscript

--- Web2/www/js/script.js
+++ Web2/www/js/script.js
@@ -1,6 +1,14 @@
 //alert("test");
 
-function controle(form1) {
-    var test = document.form1.input.value;
-    alert("Vous avez tapé : " + test);
+var nom = document.getElementById("lastname");
+var pnom = document.getElementById("firsname");
+var mail = document.getElementById("useremail");
+var mdp = document.getElementById("usrpwd");
+var pseudo = document.getElementById("usrname");
+var datnais = document.getElementById("birthdate");  
+
+function checkForm() { 
+
 }
+
+lastname.addEventListener("click", function(){alert("test");});
