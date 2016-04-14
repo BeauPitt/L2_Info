4630341aa08d856f2b827a05310f57644591aac9
BeauPitt <loriscrc@gmail.com>
2016-04-14 12:26:10 +0200

    postervideo

--- Web2/www/js/items.js
+++ Web2/www/js/items.js
@@ -5,14 +5,12 @@
 var element = document.getElementById("boomerang");
 var asideBoo = document.getElementById("span_boomerang");
 
-<<<<<<< HEAD
 function clique() {
        asideBoo.style.display = "block";
     };
 
 element.addEventListener("click", clique);
-=======
+
 element.onclick = (function() {
         alert("Vous m'avez cliqué !");
 });
->>>>>>> origin/master
