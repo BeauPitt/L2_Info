dae83e65cac2965e0c700a355621dfaf09722e5c
Lylianisking <lylian.blaud@hotmail.fr>
2016-05-01 15:12:10 +0200

    oui

--- POO/WallStreet/Banque.java
+++ POO/WallStreet/Banque.java
@@ -8,6 +8,8 @@ class Banque {
 	public Banque() {
 		th1 = new Hashtable <Integer, String> ();
 		th2 = new Hashtable <Integer, Integer> ();
+		
+		// Création des comptes
 		th1.put(1, "jean");
 		th1.put(2, "ouioui");
 		th1.put(3, "Non");
@@ -16,26 +18,37 @@ class Banque {
 		th2.put(3,400);
 	}
 
+	// Fonction qui crédite un compte
+	// Cette fonction vérifie si le proprietaire du compte a un solde inferieur au plafond avant de lui ajouter
 	public void crediterCompte(int compte, int somme) {
 		try {
 			th1.containsValue(compte);
 		} catch(IllegalArgumentException e) {
 			System.out.println("Caught an IllegalArgumentException..." + e.getMessage());
 		}
-
-		th2.put(compte, th2.get(compte) + somme);
+		
+		if( th2.get(compte) + somme < plafond) {
+			th2.put(compte, th2.get(compte) + somme);
+		}
+		
 	}
 
+	//Fonction qui debite un compte
+	// Elle verifie si le compte associé a un solde superieur a 0
 	public void debiterCompte(int compte, int somme) {
 		try {
 			th1.containsValue(compte);
 		} catch(IllegalArgumentException e) {
 			System.out.println("Caught an IllegalArgumentException..." + e.getMessage());
 		}
-
-		th2.put(compte,th2.get(compte) - somme);
+		
+		if(th2.get(compte) - somme > 0) {
+			th2.put(compte,th2.get(compte) - somme);
+		}
+		
 	}
 
+	// Retourne le nom du proprietaire du compte
 	public String proprio(int compte) {
 		try {
 			th1.containsValue(compte);
@@ -46,7 +59,7 @@ class Banque {
 		return th1.get(compte);
 	}
 
-
+	// Fait un virement entre deux compte
 	public void virement(int compte, int compte2, int somme) {
 		try{
 			th1.containsValue(compte);
@@ -54,8 +67,19 @@ class Banque {
 		} catch(IllegalArgumentException e) {
 			System.out.println("Caught an IllegalArgumentException..." + e.getMessage());
 		}
+		
+		if(th2.get(compte) - somme > 0 && th2.get(compte2) + somme < plafond) {
+			th2.put(compte, th2.get(compte) - somme);
+			th2.put(compte2, th2.get(compte2) + somme);
+		}
 
-		th2.put(compte, th2.get(compte) - somme);
-		th2.put(compte2, th2.get(compte2) + somme);
+	}
+	
+	// Affiche tout les comptes
+	public void afficherCompte() {
+		for(int i=1; i<=3; i++) {
+			System.out.println("Compte de" + th1.get(i));
+			System.out.println("Solde :" + th2.get(i));
+		}
 	}
 }
--- POO/WallStreet/Main.java
+++ POO/WallStreet/Main.java
@@ -1,7 +1,12 @@
 class Main {
 	public static void main(String[] args) {
 		Banque b = new Banque();
-		System.out.println(b.proprio(2));
-		b.
+		b.afficherCompte();
+		b.crediterCompte(1, 500);
+		b.afficherCompte();
+		b.debiterCompte(2, 1000);
+		b.afficherCompte();
+		b.virement(3, 2, 100);
+		b.afficherCompte();
 	}
 }
